35162eccc1ffc4a6027620333fb9d4ae8669da0b
Masami Yonehara <zeitdiebe@gmail.com>
2013-12-27 21:50:21 +0900

    fix a bug in cron schedule

--- config/schedule.rb
+++ config/schedule.rb
@@ -7,5 +7,5 @@ set :output, "~/cron_log.log"
 
 # utc
 every 1.day, :at => '5:10 am' do
-  command "cd /home/yukkuri/yukkuri-crawler/releases/current && RAILS_ENV=production bundle exec rake daily ENV=production"
+  command "cd /home/yukkuri/yukkuri-crawler/releases/current && bundle exec rake daily ENV=production"
 end
