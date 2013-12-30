39a5214484faec451f21514c26c9ef9403638dc0
Masami Yonehara <zeitdiebe@gmail.com>
2013-12-30 17:54:28 +0900

    refactor spec

--- spec/crawler_spec.rb
+++ spec/crawler_spec.rb
@@ -10,14 +10,12 @@ describe Crawler do
 
       before do
         # first page
-        WebMock.stub_request(:get, "http://www.nicovideo.jp/tag/%E3%82%86%E3%81%A3%E3%81%8F%E3%82%8A%E5%AE%9F%E6%B3%81%E3%83%97%E3%83%AC%E3%82%A4part1%E3%83%AA%E3%83%B3%E3%82%AF%20or%20VOICEROID%E5%AE%9F%E6%B3%81%E3%83%97%E3%83%AC%E3%82%A4Part1%E3%83%AA%E3%83%B3%E3%82%AF?numbers=1&order=d&page=1&rss=2.0&sort=f").
-          with(:headers => {'Accept'=>'*/*; q=0.5, application/xml', 'Accept-Encoding'=>'gzip, deflate', 'User-Agent'=>'Ruby'}).
-          to_return(:status => 200, :body => Fixture.part_one_movies_from_20130901_to_20130905_page1, :headers => {})
+        WebMock.stub_request(:get, "http://www.nicovideo.jp/tag/%E3%82%86%E3%81%A3%E3%81%8F%E3%82%8A%E5%AE%9F%E6%B3%81%E3%83%97%E3%83%AC%E3%82%A4part1%E3%83%AA%E3%83%B3%E3%82%AF%20or%20VOICEROID%E5%AE%9F%E6%B3%81%E3%83%97%E3%83%AC%E3%82%A4Part1%E3%83%AA%E3%83%B3%E3%82%AF?numbers=1&order=d&page=1&rss=2.0&sort=f")
+          .to_return(status: 200, body: Fixture.part_one_movies_from_20130901_to_20130905_page1)
 
         # second page
-        WebMock.stub_request(:get, "http://www.nicovideo.jp/tag/%E3%82%86%E3%81%A3%E3%81%8F%E3%82%8A%E5%AE%9F%E6%B3%81%E3%83%97%E3%83%AC%E3%82%A4part1%E3%83%AA%E3%83%B3%E3%82%AF%20or%20VOICEROID%E5%AE%9F%E6%B3%81%E3%83%97%E3%83%AC%E3%82%A4Part1%E3%83%AA%E3%83%B3%E3%82%AF?numbers=1&order=d&page=2&rss=2.0&sort=f").
-          with(:headers => {'Accept'=>'*/*; q=0.5, application/xml', 'Accept-Encoding'=>'gzip, deflate', 'User-Agent'=>'Ruby'}).
-          to_return(:status => 200, :body => Fixture.part_one_movies_from_20130901_to_20130905_page2, :headers => {})
+        WebMock.stub_request(:get, "http://www.nicovideo.jp/tag/%E3%82%86%E3%81%A3%E3%81%8F%E3%82%8A%E5%AE%9F%E6%B3%81%E3%83%97%E3%83%AC%E3%82%A4part1%E3%83%AA%E3%83%B3%E3%82%AF%20or%20VOICEROID%E5%AE%9F%E6%B3%81%E3%83%97%E3%83%AC%E3%82%A4Part1%E3%83%AA%E3%83%B3%E3%82%AF?numbers=1&order=d&page=2&rss=2.0&sort=f")
+          .to_return(status: 200, body: Fixture.part_one_movies_from_20130901_to_20130905_page2)
 
         Crawler.get_latest_part1_movie_from_web
       end
@@ -29,7 +27,7 @@ describe Crawler do
       end
     end
   end
-  
+
   describe ".retrieve_series_mylist" do
     context "when target mylist contains some movie" do
       let!(:movie) { FactoryGirl.create(:part_one_movie,  video_id: 'sm1001',
@@ -38,40 +36,32 @@ describe Crawler do
 
       before do
         # TODO ?がURLの末尾に付くのは想定外なので、nicoqueryを修正する。
-        WebMock.stub_request(:get, "http://ext.nicovideo.jp/api/getthumbinfo/sm1001?").
-          with(:headers => {'Accept'=>'*/*; q=0.5, application/xml', 'Accept-Encoding'=>'gzip, deflate', 'User-Agent'=>'Ruby'}).
-          to_return(:status => 200, :body => Fixture.getthumbinfo_sm1001, :headers => {})
+        WebMock.stub_request(:get, "http://ext.nicovideo.jp/api/getthumbinfo/sm1001?")
+          .to_return(status: 200, body: Fixture.getthumbinfo_sm1001)
 
-        WebMock.stub_request(:get, "http://ext.nicovideo.jp/api/getthumbinfo/sm1002?").
-          with(:headers => {'Accept'=>'*/*; q=0.5, application/xml', 'Accept-Encoding'=>'gzip, deflate', 'User-Agent'=>'Ruby'}).
-          to_return(:status => 200, :body => Fixture.getthumbinfo_sm1002, :headers => {})
+        WebMock.stub_request(:get, "http://ext.nicovideo.jp/api/getthumbinfo/sm1002?")
+          .to_return(status: 200, body: Fixture.getthumbinfo_sm1002)
 
-        WebMock.stub_request(:get, "http://ext.nicovideo.jp/api/getthumbinfo/sm1003?").
-          with(:headers => {'Accept'=>'*/*; q=0.5, application/xml', 'Accept-Encoding'=>'gzip, deflate', 'User-Agent'=>'Ruby'}).
-          to_return(:status => 200, :body => Fixture.getthumbinfo_sm1003, :headers => {})
+        WebMock.stub_request(:get, "http://ext.nicovideo.jp/api/getthumbinfo/sm1003?")
+          .to_return(status: 200, body: Fixture.getthumbinfo_sm1003)
 
-        WebMock.stub_request(:get, "http://ext.nicovideo.jp/api/getthumbinfo/sm2001?").
-          with(:headers => {'Accept'=>'*/*; q=0.5, application/xml', 'Accept-Encoding'=>'gzip, deflate', 'User-Agent'=>'Ruby'}).
-          to_return(:status => 200, :body => Fixture.getthumbinfo_sm2001, :headers => {})
+        WebMock.stub_request(:get, "http://ext.nicovideo.jp/api/getthumbinfo/sm2001?")
+          .to_return(status: 200, body: Fixture.getthumbinfo_sm2001)
 
-        WebMock.stub_request(:get, "http://ext.nicovideo.jp/api/getthumbinfo/sm2002?").
-          with(:headers => {'Accept'=>'*/*; q=0.5, application/xml', 'Accept-Encoding'=>'gzip, deflate', 'User-Agent'=>'Ruby'}).
-          to_return(:status => 200, :body => Fixture.getthumbinfo_sm2002, :headers => {})
+        WebMock.stub_request(:get, "http://ext.nicovideo.jp/api/getthumbinfo/sm2002?")
+          .to_return(status: 200, body: Fixture.getthumbinfo_sm2002)
 
-        WebMock.stub_request(:get, "http://ext.nicovideo.jp/api/getthumbinfo/sm2003?").
-          with(:headers => {'Accept'=>'*/*; q=0.5, application/xml', 'Accept-Encoding'=>'gzip, deflate', 'User-Agent'=>'Ruby'}).
-          to_return(:status => 200, :body => Fixture.getthumbinfo_sm2003, :headers => {})
+        WebMock.stub_request(:get, "http://ext.nicovideo.jp/api/getthumbinfo/sm2003?")
+          .to_return(status: 200, body: Fixture.getthumbinfo_sm2003)
 
         # sm1001の説明文に記載されたマイリストのURL
-        WebMock.stub_request(:get, "http://www.nicovideo.jp/mylist/1001?numbers=1&rss=2.0").
-          with(:headers => {'Accept'=>'*/*; q=0.5, application/xml', 'Accept-Encoding'=>'gzip, deflate', 'User-Agent'=>'Ruby'}).
-          to_return(:status => 200, :body => Fixture.mylist_1001, :headers => {})
+        WebMock.stub_request(:get, "http://www.nicovideo.jp/mylist/1001?numbers=1&rss=2.0")
+          .to_return(status: 200, body: Fixture.mylist_1001)
 
-        WebMock.stub_request(:get, "http://www.nicovideo.jp/mylist/2001?numbers=1&rss=2.0").
-          with(:headers => {'Accept'=>'*/*; q=0.5, application/xml', 'Accept-Encoding'=>'gzip, deflate', 'User-Agent'=>'Ruby'}).
-          to_return(:status => 200, :body => Fixture.mylist_2001, :headers => {})
+        WebMock.stub_request(:get, "http://www.nicovideo.jp/mylist/2001?numbers=1&rss=2.0")
+          .to_return(status: 200, body: Fixture.mylist_2001)
 
-        Crawler.retrieve_series_mylists
+        PartOneMovie.retrieve_series_mylist
       end
 
       subject { PartOneMovie.first }
@@ -86,9 +76,8 @@ describe Crawler do
                                                           publish_date: Time.local(2013, 9, 1, 0, 0, 0)) }
 
       before do
-        WebMock.stub_request(:get, "http://ext.nicovideo.jp/api/getthumbinfo/sm3001?").
-          with(:headers => {'Accept'=>'*/*; q=0.5, application/xml', 'Accept-Encoding'=>'gzip, deflate', 'User-Agent'=>'Ruby'}).
-          to_return(:status => 200, :body => Fixture.getthumbinfo_deleted, :headers => {})
+        WebMock.stub_request(:get, "http://ext.nicovideo.jp/api/getthumbinfo/sm3001?")
+          .to_return(status: 200, body: Fixture.getthumbinfo_deleted)
       end
     end
   end
