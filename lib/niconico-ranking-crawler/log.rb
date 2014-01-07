68267344483bbad31e6ca7fcb9fe8a40e0d4132d
Masami Yonehara <zeitdiebe@gmail.com>
2014-01-07 09:32:52 +0900

    fix logger's bug

--- lib/niconico-ranking-crawler/log.rb
+++ lib/niconico-ranking-crawler/log.rb
@@ -15,11 +15,15 @@ class Log
   end
 
   def log_file
-    env = test_env? ? "test" : [ENV["NICO_CRAWLER_ENV"]]
+    env = test_env? ? "test" : specified_env
     File.open(YAML.load_file("config/crawler.yml")["log_file_path"][env], "a")
   end
 
   def test_env?
     $test_env == true
+  end
+
+  def specified_env
+    ENV["NICO_CRAWLER_ENV"].presence || "local"
   end
 end
