2ae2a75e82937941063ad655f6b06a847812d58d
Masami Yonehara <zeitdiebe@gmail.com>
2014-01-31 18:47:39 +0900

    fix whenever

--- config/schedule.rb
+++ config/schedule.rb
@@ -3,9 +3,9 @@
 # It's helpful, but not entirely necessary to understand cron before proceeding.
 # http://en.wikipedia.org/wiki/Cron
 
-set :output, "~/cron_log.log"
+# set :output, "/var/log/cron_log.log"
 
 # utc
 every 1.day, :at => '5:10 pm' do
-  command "cd /home/yukkuri/yukkuri-crawler/current && bundle exec rake crawl:daily NICO_CRAWLER_ENV=production"
+  command "cd /yukkuri-crawler/current && bundle exec rake crawl:daily"
 end
