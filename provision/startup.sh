4a4fd0916537b6fe530fd31a900b51188a38979e
Masami Yonehara <zeitdiebe@gmail.com>
2014-02-28 22:15:58 +0900

    add a command to startup.sh to start cron

--- provision/startup.sh
+++ provision/startup.sh
@@ -5,3 +5,4 @@ git clone https://github.com/hdemon/yukkuri-ranking-aggregator.git
 cd yukkuri-ranking-aggregator && bundle install -j4
 
 echo "10 17 * * * /bin/bash -l -c 'cd /home/app/yukkuri-ranking-aggregator && bundle exec rake crawl:daily >> /var/log/cron_log.log 2>&1'" >> /etc/cron.d/crawler
+cron -f
