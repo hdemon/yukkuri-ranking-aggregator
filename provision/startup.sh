3bbd18021308ba8eea3d570fe1f6c57213432310
Masami Yonehara <zeitdiebe@gmail.com>
2014-02-28 23:32:22 +0900

    fix bugs about cron task

--- provision/startup.sh
+++ provision/startup.sh
@@ -1,8 +1,11 @@
 #!/bin/sh
 export PATH=$PATH:/usr/ruby/bin
 
+su app
+cd /home/app
 git clone https://github.com/hdemon/yukkuri-ranking-aggregator.git
-cd yukkuri-ranking-aggregator && bundle install -j4
+cd yukkuri-ranking-aggregator
+bundle install -j4
 
-echo "10 17 * * * /bin/bash -l -c 'cd /home/app/yukkuri-ranking-aggregator && bundle exec rake crawl:daily >> /var/log/cron_log.log 2>&1'" >> /etc/cron.d/crawler
-cron -f
+echo "10 17 * * * /bin/bash -l -c 'cd /home/app/yukkuri-ranking-aggregator && bundle exec rake crawl:daily >> /var/log/cron_log.log 2>&1'" >> /var/spool/cron/crontabs/app
+cron
