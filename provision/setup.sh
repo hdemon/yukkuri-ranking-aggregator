d741073287dc145703e108eef20a4fb440009117
Masami Yonehara <zeitdiebe@gmail.com>
2014-02-28 19:57:18 +0900

    Fix bugs in Dockerfile

--- provision/setup.sh
+++ provision/setup.sh
@@ -4,4 +4,4 @@ export PATH=$PATH:/usr/ruby/bin
 git clone https://github.com/hdemon/yukkuri-ranking-aggregator.git
 cd yukkuri-ranking-aggregator && bundle install -j4
 
-echo "10 17 * * * /bin/bash -l -c 'cd /yukkuri-ranking-aggregator && bundle exec rake crawl:daily >> /var/log/cron_log.log 2>&1'" >> /etc/cron.d/crawler
+echo "10 17 * * * /bin/bash -l -c 'cd /home/app/yukkuri-ranking-aggregator && bundle exec rake crawl:daily >> /var/log/cron_log.log 2>&1'" >> /etc/cron.d/crawler
