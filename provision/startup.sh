ad8766e1629777cfcbbf4e4c88a86d89814692e9
Masami Yonehara <zeitdiebe@gmail.com>
2014-03-01 22:26:03 +0900

    fix bugs

--- provision/startup.sh
+++ provision/startup.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
-export PATH=$PATH:/usr/ruby/bin
+export PATH=/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/ruby/bin:/home/app/.rbenv/bin:/home/app/.rbenv/shims:/home/app/.rbenv/plugins
 
 cd /home/app
 git clone https://github.com/hdemon/yukkuri-ranking-aggregator.git
 cd yukkuri-ranking-aggregator
-bundle install -j4
+rbenv exec bundle --path vendor/bundle -j4
 
 echo "10 17 * * * /bin/bash -l -c 'cd /home/app/yukkuri-ranking-aggregator && bundle exec rake crawl:daily >> /var/log/cron_log.log 2>&1'" >> /var/spool/cron/crontabs/app
 cron
