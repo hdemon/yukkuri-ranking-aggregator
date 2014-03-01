5fd20544e1eef021cc7d91324853d8e6c118875f
Masami Yonehara <zeitdiebe@gmail.com>
2014-03-02 01:37:40 +0900

    add config files for docker provisioning

--- provision/startup.sh
+++ provision/startup.sh
@@ -2,7 +2,6 @@
 export PATH=/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/ruby/bin:/home/app/.rbenv/bin:/home/app/.rbenv/shims:/home/app/.rbenv/plugins
 
 cd /home/app/yukkuri-ranking-aggregator
-cat config/config.yml
 rbenv exec bundle exec rake db:create
 rbenv exec bundle exec rake db:migrate
 
