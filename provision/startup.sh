58e0b1d8048e4df6354f261f340e6fd4d73d0701
Masami Yonehara <zeitdiebe@gmail.com>
2014-03-29 15:14:19 +0900

    Revert "refactor Dockerfile"
    This reverts commit 4e03fa64c1a6b5216f082bbe0939508c3cd1bce8.
    
    Conflicts:
    	provision/startup.sh

--- provision/startup.sh
+++ provision/startup.sh
@@ -1,8 +1,12 @@
 #!/bin/sh
 export PATH=/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/ruby/bin:/home/app/.rbenv/bin:/home/app/.rbenv/shims:/home/app/.rbenv/plugins
 
+cd /home/app
+git clone https://github.com/hdemon/yukkuri-ranking-aggregator.git
 cd /home/app/yukkuri-ranking-aggregator
+rbenv exec bundle --path vendor/bundle -j4
 rbenv exec bundle exec rake db:create
 rbenv exec bundle exec rake db:migrate
 
+echo "10 17 * * * /bin/bash -l -c 'cd /home/app/yukkuri-ranking-aggregator && bundle exec rake crawl:daily >> /var/log/cron_log.log 2>&1'" >> /var/spool/cron/crontabs/app
 cron -f
