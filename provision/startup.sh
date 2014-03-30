90f6a569504d826bf86188e76b20c511c85dc0b3
Masami Yonehara <zeitdiebe@gmail.com>
2014-03-30 14:34:07 +0900

    modified dockerfile

--- provision/startup.sh
+++ provision/startup.sh
@@ -3,10 +3,12 @@ export PATH=/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/ru
 
 cd /home/app
 git clone https://github.com/hdemon/yukkuri-ranking-aggregator.git
+cp /tmp/database.yml /home/app/yukkuri-ranking-aggregator/config/database.yml
 cd /home/app/yukkuri-ranking-aggregator
 rbenv exec bundle --path vendor/bundle -j4
-rbenv exec bundle exec rake db:create
-rbenv exec bundle exec rake db:migrate
+rbenv exec bundle exec rake db:create RAILS_ENV=production
+rbenv exec bundle exec rake db:migrate RAILS_ENV=production
 
-echo "10 17 * * * /bin/bash -l -c 'cd /home/app/yukkuri-ranking-aggregator && bundle exec rake crawl:daily >> /var/log/cron_log.log 2>&1'" >> /var/spool/cron/crontabs/app
+sudo mkdir -p /var/spool/cron/crontabs
+sudo echo "10 17 * * * /bin/bash -l -c 'cd /home/app/yukkuri-ranking-aggregator && bundle exec rake crawl:daily >> /var/log/cron_log.log 2>&1'" >> /var/spool/cron/crontabs/app
 cron -f
